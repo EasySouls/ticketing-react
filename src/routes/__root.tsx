33c9a1c8cec5d289261398afebf6114b87e26ae6
EasySouls <tarjanyicsanad@gmail.com>
2024-03-19 18:58:49 +0100

    Add shadcn-ui

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,7 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router';
 import Header from '../components/Header';
 import React, { Suspense } from 'react';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 const TanStackRouterDevtools =
   import.meta.env.NODE_ENV === 'production'
@@ -18,6 +19,7 @@ export const Route = createRootRoute({
       <Outlet />
       <Suspense>
         <TanStackRouterDevtools />
+        <ReactQueryDevtools />
       </Suspense>
     </>
   ),
